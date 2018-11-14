60a02380559a40f2a88eb3cc1da29cfb95406609
sammienjihia <dilaninjihia@gmail.com>
2018-11-14 21:06:35 +0300

    Not so efficient custom KNN algorithm

--- sample_KNN.py
+++ sample_KNN.py
@@ -40,4 +40,19 @@ print(pd_TSindex.nsmallest(K, columns='E.Distance'))
 print(pd_data['Label'].mode())
 
 
- 
+def KNN(k, target, sample_size, samples, number_features, pd_dataframe, new_column_name, label_column):
+ 	e_distance = []
+ 	for rows in range(sample_size):
+ 		e_distance.append(euclidean(target[0], samples[rows], number_features))
+
+ 	pd_dataframe[new_column_name] = e_distance	
+
+ 	pd_data = pd_TSindex.nsmallest(K, columns=new_column_name)
+
+ 	return pd_data[label_column].mode()
+
+
+new_column_name='E.Distance'; label_column = 'Label'
+
+print(KNN(3, [[161,61]], len(np_TSindex[:]), np_TSindex, 2, pd_TSindex, new_column_name, label_column))
+
